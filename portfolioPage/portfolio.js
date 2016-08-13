6f1f8dd23e701d13d4fd5988f57ed54991f0b324
gaosams <gaosams@gmail.com>
2016-08-13 13:51:37 -0400

    Scroll: Highlight Nav
    Added javascript to highlight relevant navigation element when
    scrolling.

--- portfolioPage/portfolio.js
+++ portfolioPage/portfolio.js
@@ -1,13 +1,46 @@
 $(document).ready(function() {
-	if (true){
-		console.log(true);
+	// check in javascript is running
+	console.log("Javascript Running");
+	
+	// for selecting navigation items
+	var portfolio = 'a[href="#portfolio"]';
+	var aboutMe = 'a[href="#aboutMe"]';
+	var resume = 'a[href="#resume"]';
+
+	// position of each section
+	var navHeight = 100;
+	var portfolioTop = $('#portfolio').position().top - navHeight;
+	var aboutMeTop = $('#aboutMe').position().top - navHeight;
+
+	function defineTopPos() {
+		var portfolioTop = $('#portfolio').position().top;
+		var aboutMeTop = $('#aboutMe').position().top;
 	}
 
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 200) {
-			$('body').fadeIn(500);
-			console.log(document.body.scrollTop);
+	//when window resized, run code
+	$(window).resize(function() {
+		defineTopPos();
+	})
+
+
+	function changeNavHighlight(section) {
+		$('#myNavBar>ul>li>a.active').removeClass('active');
+		$(section).addClass('active');
+	}
+
+
+	//on scroll, run code
+	$(window).scroll(function() {
+		console.log($(this).scrollTop());
+		if ($(this).scrollTop() < portfolioTop) {
+			$('#myNavBar>ul>li>a.active').removeClass('active');
+		} else if ($(this).scrollTop() >= portfolioTop && 
+			$(this).scrollTop() < aboutMeTop) {
+			changeNavHighlight(portfolio);
+		} else if ($(this).scrollTop() >= aboutMeTop) {
+			changeNavHighlight(aboutMe);
 		}
+
 	})
 
 	$("#aboutMe").css("color", "green");
