76548ebea0aadb143009e0bc474baf9e8c51854c
gaosams <gaosams@gmail.com>
2016-08-18 02:28:36 -0400

    about me reformat
    about me section from circle image and single column to two column
    img+text.
    
    Adjusted submit button at the bottom.
    
    Re-aligned

--- portfolioPage/portfolio.js
+++ portfolioPage/portfolio.js
@@ -6,15 +6,19 @@ $(document).ready(function() {
 	var portfolio = 'a[href="#portfolio"]';
 	var aboutMe = 'a[href="#aboutMe"]';
 	var resume = 'a[href="#resume"]';
+	var contact = 'a[href="#contact"]';
 
 	// position of each section
 	var navHeight = 60;
 	var portfolioTop = $('#portfolio').position().top - navHeight;
 	var aboutMeTop = $('#aboutMe').position().top - navHeight;
+	var contactTop = $('#contact').position().top - navHeight;
+
 
 	function defineTopPos() {
 		var portfolioTop = $('#portfolio').position().top - navHeight;
 		var aboutMeTop = $('#aboutMe').position().top - navHeight;
+		var contactTop = $('#contact').position().top - navHeight;
 	}
 
 	//when window resized, run code
@@ -36,8 +40,11 @@ $(document).ready(function() {
 		} else if ($(this).scrollTop() >= portfolioTop && 
 			$(this).scrollTop() < aboutMeTop) {
 			changeNavHighlight(portfolio);
-		} else if ($(this).scrollTop() >= aboutMeTop) {
+		} else if ($(this).scrollTop() >= aboutMeTop &&
+			$(this).scrollTop() < contactTop) {
 			changeNavHighlight(aboutMe);
+		} else if ($(this).scrollTop() >= contactTop) {
+			changeNavHighlight(contact);
 		}
 	});
 
@@ -49,12 +56,13 @@ $(document).ready(function() {
 	});
 
 
-
+//FORM CODE
 	//form: when clicking in or out of an input field
 	$('.overlabel').each(function () {
 	    var field = $(this).find('[type=text], [type=file], [type=email], [type=password], textarea');
 	    var span = $(this).find('> span');
 	    var input = $(this).find('> input');
+	    var textarea = $(this).find('> textarea');
 	    
 
 	    //when clicking OUT of field
@@ -62,14 +70,18 @@ $(document).ready(function() {
 	    	//if something in input field upon clicking out
 	        if ($.trim(field.val()) !== '') {
 	        	//change color
+	        	textarea.css("color", "#7291d6");
 	            input.css("color", "#7291d6");
-	            //check to see if valid input
+//!!	        //check to see if valid input
+//!!	            //make a warning message appear if not valid
+
 	        }
 	    };
 
 	    //when clicking INTO a field...
 	    field.focus(function () {
 	    	input.css("color", "black");
+	    	textarea.css("color", "black");
 	    }).blur(onBlur);
 	    onBlur();
 
